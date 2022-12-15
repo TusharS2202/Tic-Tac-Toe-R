9064856635d75408ca195e0fce83313face26753
Tushar <saini.22@iitj.ac.in>
2022-12-15 16:59:55 +0530

    Updates

---
+++ precache-manifest.561cff6dcccb88d4b2a386ba9f63bd7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df003609b7009146252b",
+    "url": "/Tic-Tac-Toe-R/static/css/main.edf92880.chunk.css"
+  },
+  {
+    "revision": "df003609b7009146252b",
+    "url": "/Tic-Tac-Toe-R/static/js/main.df003609.chunk.js"
+  },
+  {
+    "revision": "7b2e37571c17e8d4fdd1",
+    "url": "/Tic-Tac-Toe-R/static/js/1.7b2e3757.chunk.js"
+  },
+  {
+    "revision": "3012558ca4b9706c6463",
+    "url": "/Tic-Tac-Toe-R/static/js/2.3012558c.chunk.js"
+  },
+  {
+    "revision": "a0fd3f508bd6ed138b77",
+    "url": "/Tic-Tac-Toe-R/static/js/runtime~main.a0fd3f50.js"
+  },
+  {
+    "revision": "66398b80ff6062e2317422ac1e7853c9",
+    "url": "/Tic-Tac-Toe-R/index.html"
+  }
+];
